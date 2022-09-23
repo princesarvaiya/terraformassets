b040c15de7447753a349b67ef221c8ec86a2ca7f
princesarvaiya <prince.tcet@gmail.com>
2022-09-23 14:09:09 -0400

    update variables json

--- hashicat/variables.tf
+++ hashicat/variables.tf
@@ -34,3 +34,16 @@ variable "admin_username" {
   description = "Administrator user name for app"
   default     = "ubuntu"
 }
+
+variable "height" {
+  default     = "400"
+  description = "Image height in pixels."
+}
+variable "width" {
+  default     = "600"
+  description = "Image width in pixels."
+}
+variable "placeholder" {
+  default     = "placekitten.com"
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+}
